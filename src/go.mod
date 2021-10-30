b971a0da0a2236a371ff98e33ee23a0261fd3980
Elliot67 <elliot.lintz@gmail.com>
2021-10-31 01:27:32 +0200

    :sparkles: add more randomness to the image

--- src/go.mod
+++ src/go.mod
@@ -5,6 +5,6 @@ go 1.17
 require (
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/jdxyw/generativeart v0.0.0-20211022125844-a2c7aca3d558 // indirect
+	github.com/jdxyw/generativeart v0.0.0-20211022125844-a2c7aca3d558
 	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
 )
--- src/main.go
+++ src/main.go
@@ -17,7 +17,6 @@ func main() {
 	stats := getStats()
 	hash := GetMD5Hash(stats)
 	rand.Seed(int64(hash - 9223372036854775808))
-
 	generateImage()
 }
 
@@ -49,36 +48,20 @@ func GetMD5Hash(text string) uint64 {
 func generateImage() {
 	c := generativeart.NewCanva(800, 350)
 	setCanvaOptions(c, OptionsOverride{
-		Background:  common.Black,
-		Foreground:  common.LightPink,
-		LineColor:   common.Lavender,
-		LineWidth:   3,
-		ColorSchema: []color.RGBA{common.Bisque, common.Mintcream},
-		NIters:      20,
-		Alpha:       255,
+		Background: common.Black,
+		LineColor:  color.RGBA{165, 174, 217, 255},
 	})
-
 	c.FillBackground()
-	c.Draw(arts.NewBlackHole(200, 900, 0.01))
+	c.Draw(arts.NewBlackHole(rand.Intn(800-100)+100, 900, 0.005+rand.Float64()*(.04-0.005)))
 	c.ToPNG("../.repo/profilehash.png")
 }
 
 type OptionsOverride struct {
-	Background  color.RGBA
-	Foreground  color.RGBA
-	LineColor   color.RGBA
-	LineWidth   float64
-	ColorSchema []color.RGBA
-	NIters      int
-	Alpha       int
+	Background color.RGBA
+	LineColor  color.RGBA
 }
 
 func setCanvaOptions(c *generativeart.Canva, options OptionsOverride) {
 	c.SetBackground(options.Background)
-	c.SetForeground(options.Foreground)
 	c.SetLineColor(options.LineColor)
-	c.SetLineWidth(options.LineWidth)
-	c.SetColorSchema(options.ColorSchema)
-	c.SetIterations(options.NIters)
-	c.SetAlpha(options.Alpha)
 }
